b8effd0258ab0bad7fadf7fb0a3e991731716413
Grano14 <granogiuseppe01@gmail.com>
2024-07-05 15:43:56 +0200

    first commit

---
+++ src/read_ds.py
@@ -0,0 +1,50 @@
+import os
+import yaml
+from PIL import Image
+from torch.utils.data import Dataset, random_split
+from torchvision import transforms
+
+class TrafficLightDataset(Dataset):
+    def __init__(self, yaml_file, transform=None):
+        with open(yaml_file, 'r') as file:
+            self.data = yaml.safe_load(file)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path = self.data[idx]['path']
+        image = Image.open(img_path).convert('RGB')
+        
+        boxes = self.data[idx]['boxes']
+        labels = []
+        bboxes = []
+        for box in boxes:
+            labels.append(box['label'])
+            bboxes.append([box['x_min'], box['y_min'], box['x_max'], box['y_max']])
+        
+        if self.transform:
+            image = self.transform(image)
+
+        return image, labels, bboxes
+
+# Esempio di utilizzo
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+#dataset = TrafficLightDataset(yaml_file='/home/giuseppe/Scaricati/train/train.yaml', transform=transform)
+
+# Determinare le dimensioni dei dataset di train e test
+#dataset_size = len(dataset)
+#train_size = int(0.8 * dataset_size)
+#test_size = dataset_size - train_size
+
+# Dividere il dataset in train e test
+#train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+# Verifica
+#print("Dimensione del training set:", len(train_dataset))
+#print("Dimensione del test set:", len(test_dataset))
---
+++ src/utils.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+import copy
+import torch
+from torchvision import datasets, transforms
+from sampling import mnist_iid, mnist_noniid, mnist_noniid_unequal
+from sampling import cifar_iid, cifar_noniid
+
+from torch.utils.data import Dataset, random_split
+import yaml
+from PIL import Image
+
+
+def get_dataset(args):
+    """ Returns train and test datasets and a user group which is a dict where
+    the keys are the user index and the values are the corresponding data for
+    each of those users.
+    """
+
+
+    #######################################
+    class TrafficLightDataset(Dataset):
+        def __init__(self, yaml_file, transform=None):
+            with open(yaml_file, 'r') as file:
+                self.data = yaml.safe_load(file)
+            self.transform = transform
+
+        def __len__(self):
+            return len(self.data)
+
+        def __getitem__(self, idx):
+            img_path = self.data[idx]['path']
+            image = Image.open(img_path).convert('RGB')
+            
+            boxes = self.data[idx]['boxes']
+            labels = []
+            for box in boxes:
+                labels.append(0)
+            
+            if self.transform:
+                image = self.transform(image)
+            
+            # Convert labels to a single label (if applicable)
+            # If multi-label classification is needed, convert to a one-hot encoding or similar representation
+            # Assuming you want to use the first label for now
+            labels = 0
+
+            return image, labels
+        
+    # Esempio di utilizzo
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    #######################################
+
+    
+    if args.dataset == 'cifar':
+        dataset = TrafficLightDataset(yaml_file='/home/giuseppe/Scaricati/train/train.yaml', transform=transform)
+
+        test_ratio = 0.2  # 20% dei dati per il set di test
+        num_total = len(dataset)
+        num_test = int(test_ratio * num_total)
+        num_train = num_total - num_test
+
+        # Dividiamo il dataset in set di addestramento e di test
+        train_dataset, test_dataset = random_split(dataset, [num_train, num_test])
+    
+        # Ora train_dataset e test_dataset sono oggetti Dataset di PyTorch
+        # contenenti rispettivamente i dati di addestramento e di test.
+
+        print("Numero di campioni nel set di addestramento:", len(train_dataset))
+        print("Numero di campioni nel set di test:", len(test_dataset))
+
+
+        # sample training data amongst users
+        if args.iid:
+            # Sample IID user data from Mnist
+            user_groups = cifar_iid(train_dataset, args.num_users)
+        else:
+            # Sample Non-IID user data from Mnist
+            if args.unequal:
+                # Chose uneuqal splits for every user
+                raise NotImplementedError()
+            else:
+                # Chose euqal splits for every user
+                user_groups = cifar_noniid(train_dataset, args.num_users)
+
+    elif args.dataset == 'mnist' or 'fmnist':
+        if args.dataset == 'mnist':
+            data_dir = '../data/mnist/'
+        else:
+            data_dir = '../data/fmnist/'
+
+        apply_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))])
+
+        train_dataset = datasets.MNIST(data_dir, train=True, download=True,
+                                       transform=apply_transform)
+
+        test_dataset = datasets.MNIST(data_dir, train=False, download=True,
+                                      transform=apply_transform)
+
+        # sample training data amongst users
+        if args.iid:
+            # Sample IID user data from Mnist
+            user_groups = mnist_iid(train_dataset, args.num_users)
+        else:
+            # Sample Non-IID user data from Mnist
+            if args.unequal:
+                # Chose uneuqal splits for every user
+                user_groups = mnist_noniid_unequal(train_dataset, args.num_users)
+            else:
+                # Chose euqal splits for every user
+                user_groups = mnist_noniid(train_dataset, args.num_users)
+
+    return train_dataset, test_dataset, user_groups
+
+
+def average_weights(w):
+    """
+    Returns the average of the weights.
+    """
+    w_avg = copy.deepcopy(w[0])
+    for key in w_avg.keys():
+        for i in range(1, len(w)):
+            w_avg[key] += w[i][key]
+        w_avg[key] = torch.div(w_avg[key], len(w))
+    return w_avg
+
+
+def exp_details(args):
+    print('\nExperimental details:')
+    print(f'    Model     : {args.model}')
+    print(f'    Optimizer : {args.optimizer}')
+    print(f'    Learning  : {args.lr}')
+    print(f'    Global Rounds   : {args.epochs}\n')
+
+    print('    Federated parameters:')
+    if args.iid:
+        print('    IID')
+    else:
+        print('    Non-IID')
+    print(f'    Fraction of users  : {args.frac}')
+    print(f'    Local Batch size   : {args.local_bs}')
+    print(f'    Local Epochs       : {args.local_ep}\n')
+    return
