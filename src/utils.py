6b95c6772c2322b0bd5248041113ce4b3df5c4da
Grano14 <granogiuseppe01@gmail.com>
2024-07-09 12:52:28 +0200

    Null commit

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@
 
 import copy
 import torch
-#from torchvision import datasets, transforms
+from torchvision import datasets, transforms
 from sampling import mnist_iid, mnist_noniid, mnist_noniid_unequal
 from sampling import cifar_iid, cifar_noniid
 from datasets import load_dataset
